96de316d73c64e0ae0f5e3c5df4518dd84353349
inf18 <38174595+inf18@users.noreply.github.com>
2018-04-09 01:29:21 +0700

    Add files via upload

---
+++ Perpus/src/Perpus/Perpustakaan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Perpus;
+
+/**
+ *
+ * @author acer
+ */
+public class Perpustakaan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login tampil = new Login();
+        tampil.setVisible(true);
+    }
+}
